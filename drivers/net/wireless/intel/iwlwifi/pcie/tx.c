greearb/linux-ct-6.10

wifi: iwlmvm: Add debugfs file for simulating firmware double-fault.
This can be used to reproduce a specific scenario where the firmware
crashes, and then fails to recover. This has been problematic in the
past.

Signed-off-by: Rory Little <rory@candelatech.com>

drivers/net/wireless/intel/iwlwifi/pcie/tx.c
@@ -2567,10 +2567,17 @@ static int iwl_trans_pcie_send_hcmd_sync(struct iwl_trans *trans,
 		return ret;
 	}
 
-	ret = wait_event_timeout(trans->wait_command_queue,
-				 !test_bit(STATUS_SYNC_HCMD_ACTIVE,
-					   &trans->status),
-				 HOST_COMPLETE_TIMEOUT);
+	if (unlikely(trans->dbg.fake_double_fault)) {
+		ret = 0;
+		iwl_force_nmi(trans);
+		IWL_ERR(trans, "Triggering false firmware timeout from debugfs flag\n");
+	} else {
+		ret = wait_event_timeout(trans->wait_command_queue,
+					 !test_bit(STATUS_SYNC_HCMD_ACTIVE,
+						   &trans->status),
+					 HOST_COMPLETE_TIMEOUT);
+	}
+
 	if (!ret) {
 		IWL_ERR(trans, "Error sending %s: time out after %dms.\n",
 			cmd_str, jiffies_to_msecs(HOST_COMPLETE_TIMEOUT));

greearb/linux-ct-6.16

drivers/net/wireless/intel/iwlwifi/pcie/tx.c
@@ -2581,10 +2581,17 @@ static int iwl_trans_pcie_send_hcmd_sync(struct iwl_trans *trans,
 		return ret;
 	}
 
-	ret = wait_event_timeout(trans_pcie->wait_command_queue,
-				 !test_bit(STATUS_SYNC_HCMD_ACTIVE,
-					   &trans->status),
-				 HOST_COMPLETE_TIMEOUT);
+	if (unlikely(trans->dbg.fake_double_fault)) {
+		ret = 0;
+		iwl_force_nmi(trans);
+		IWL_ERR(trans, "Triggering false firmware timeout from debugfs flag\n");
+	} else {
+		ret = wait_event_timeout(trans_pcie->wait_command_queue,
+					 !test_bit(STATUS_SYNC_HCMD_ACTIVE,
+						   &trans->status),
+					 HOST_COMPLETE_TIMEOUT);
+	}
+
 	if (!ret) {
 		IWL_ERR(trans, "Error sending %s: time out after %dms.\n",
 			cmd_str, jiffies_to_msecs(HOST_COMPLETE_TIMEOUT));